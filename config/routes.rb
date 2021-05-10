17cf7c0aa6a25287348e5df695588731dbe3738d
kirua <yo44prg@icloud.com>
2021-05-10 22:47:42 +0200

    feat: add login controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get "/trend" => "trend#send_data"
 
   get "/advert" => "advert#send_image_carousel"
+
+  # get "/api/stories/tag/:tag" => "api/tags#find_stories_by_tags"
+
+  get "/login/:log" => "login#find_logs"
 end
