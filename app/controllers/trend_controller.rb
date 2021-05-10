920d3d09432bc63863f3e9adafb2ea005fa20097
kirua <yo44prg@icloud.com>
2021-05-10 09:54:13 +0200

    feat: modify image path

--- app/controllers/trend_controller.rb
+++ app/controllers/trend_controller.rb
@@ -2,28 +2,28 @@ class TrendController < ApplicationController
   def initialize
     @data = [
       {
-        image_path: "http://localhost:3000/images/sneakers/adidas-ozweego-celox.png",
+        image_path: "https://scrapysneake.herokuapp.com/images/sneakers/adidas-ozweego-celox.png",
         model: "Adidas Ozweego Celox",
         price: "120,00 €",
         gender: "Mixte",
         seller_name: "courir"
       },
       {
-        image_path: "http://localhost:3000/images/sneakers/air-jordan-one.png",
+        image_path: "https://scrapysneake.herokuapp.com/images/sneakers/air-jordan-one.png",
         model: "Jordan Air One",
         price: "130,00 €",
         gender: "Homme",
         seller_name: "courir"
       },
       {
-        image_path: "http://localhost:3000/images/sneakers/new-balance-327.png",
+        image_path: "https://scrapysneake.herokuapp.com/images/sneakers/new-balance-327.png",
         model: "New Balance 327",
         price: "90,00 €",
         gender: "Femme",
         seller_name: "courir"
       },
       {
-        image_path: "http://localhost:3000/images/sneakers/puma-mirage-tech.png",
+        image_path: "https://scrapysneake.herokuapp.com/images/sneakers/puma-mirage-tech.png",
         model: "Puma Mirage Tech",
         price: "100,00 €",
         gender: "Homme",
