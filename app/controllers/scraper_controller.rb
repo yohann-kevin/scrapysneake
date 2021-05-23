d63b9d8135f14ca95cf20259958fc8d40319de8c
kirua <yo44prg@icloud.com>
2021-05-23 14:05:27 +0200

    feat: add scraper for foot locker

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -1,8 +1,12 @@
 class ScraperController < ApplicationController
   def scrap
     # ScraperCourirController.scrap_courir
-    ScraperFootlockerController.scrap_men_sneakers
-
-    render json: {"plop" => "plop"}
+    # foot_locker_sneaker = ScraperFootlockerController.scrap_foot_locker
+    puts " "
+    puts foot_locker_sneaker.length
+    render json: {"data" => sneaker = {
+        "foot_locker" => "foot_locker_sneaker"
+      }
+    }
   end
 end
--- app/controllers/scraper_footlocker_controller.rb
+++ app/controllers/scraper_footlocker_controller.rb
@@ -3,49 +3,63 @@ require "open-uri"
 
 class ScraperFootlockerController < ApplicationController
   def self.scrap_foot_locker
-    
+    @sneaker_foot_locker = []
+    scrap_man_sneakers
+    scrap_women_sneakers
   end
 
-  def self.scrap_men_sneakers
-    men_page_url = "https://www.footlocker.fr/fr/category/homme/chaussures.html"
-    men_page = URI.open(men_page_url)
-    men_page_html = Nokogiri::HTML(men_page)
-    find_men_sneakers(men_page_html)
+  def self.scrap_man_sneakers
+    man_page_url = "https://www.footlocker.fr/fr/category/homme/chaussures.html"
+    man_page = URI.open(man_page_url)
+    man_page_html = Nokogiri::HTML(man_page)
+    find_sneakers(man_page_html, "man")
   end
 
-  def self.find_men_sneakers(page)
+  def self.scrap_women_sneakers
+    women_page_url = "https://www.footlocker.fr/fr/category/femme/chaussures.html"
+    women_page = URI.open(women_page_url)
+    women_page_html = Nokogiri::HTML(women_page)
+    find_sneakers(women_page_html, "women")
+  end
+
+  def self.find_sneakers(page, gender)
     initial_url = "https://www.footlocker.fr"
     page_size = page.css(".product-container").length
-    page = page.css(".product-container")[47]
-    model = page.css(".ProductName-primary").text
-    price = page.css(".ProductPrice").text
-    link = page.css(".ProductCard-link")[0]["href"]
-    image = find_image("https://www.footlocker.fr/", link)
-
-    # //images.footlocker.com/is/image/FLEU/
-    # id : 314520713604
-    # ?wid=236&hei=236&fmt=png-alpha
-
-    puts "-----------"
-    puts "model : #{model}"
-    puts " "
-    puts "price : #{price.tr('^0-9,', '')}"
-    puts " "
-    puts "image path : #{image}"
-    puts " "
-    puts "link : #{initial_url}#{link}"
-    puts " "
-    puts "size : #{page_size}"
+    puts "gender : #{gender}"
+    0.upto(page_size - 1) {
+      |el|
+      puts el
+      section = page.css(".product-container")[el]
+      product_link = section.css(".ProductCard-link")[0]["href"]
+      @sneaker_foot_locker << {
+        "model" => section.css(".ProductName-primary").text,
+        "price" => section.css(".ProductPrice").text.tr('^0-9,', ''),
+        "link" => "#{initial_url}#{product_link}",
+        "gender" => gender,
+        "seller" => "Foot Locker",
+        "image_path" => find_image("https://www.footlocker.fr/", product_link)
+      }
+    }
+    
+    return @sneaker_foot_locker
   end
 
+  # //images.footlocker.com/is/image/FLEU/
+  # id : 314520713604
+  # ?wid=236&hei=236&fmt=png-alpha
   def self.find_image(start_uri, link)
     product_page = URI.open("#{start_uri}#{link}")
     product_page = Nokogiri::HTML(product_page)
-    id = product_page.css(".Tab-panel")[0].text
-    # plop.slice(0..(plop.index('world')))
-    id = id.slice(0..(id.index("Color"))).tr('^0-9', '')
-    initial_url_img = "images.footlocker.com/is/image/FLEU/"
-    final_url_img = "?wid=236&hei=236&fmt=png-alpha"
-    uri = "#{initial_url_img}#{id}#{final_url_img}"
+    
+    begin
+      id = product_page.css(".Tab-panel")[0].text
+      id = id.slice(0..(id.index("Color"))).tr('^0-9', '')
+      initial_url_img = "images.footlocker.com/is/image/FLEU/"
+      final_url_img = "?wid=236&hei=236&fmt=png-alpha"
+      uri = "#{initial_url_img}#{id}#{final_url_img}"
+    rescue => e
+      puts e
+      uri = "error"
+    end
   end
 end
