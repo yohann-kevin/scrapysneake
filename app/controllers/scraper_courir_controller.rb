e8b736bd25ff478a8d061aa630ba0395b1cfa11b
kirua <yo44prg@icloud.com>
2021-05-25 15:16:31 +0200

    feat: add url for sneaker seller

--- app/controllers/scraper_courir_controller.rb
+++ app/controllers/scraper_courir_controller.rb
@@ -7,7 +7,19 @@ class ScraperCourirController < ApplicationController
     home_page_url = "https://www.courir.com/fr/c/homme/?prefn1=typology&prefv1=SNEAKERS"
     # home_page_url = "https://www.footlocker.fr/" scraper one page !
     # home_page_url = "https://www.chausport.com/" scraper one page !
-    # home_page_url = "https://www.laboutiqueofficielle.com/" valider !
+    # home_page_url = "https://www.laboutiqueofficielle.com/" scraper one page !
+    
+    # site à scraper : 
+    # https://www.cornerstreet.fr/sneakers/voir-tous-les-produits.html?p=3
+    # https://www.footpatrol.fr/chaussures/
+    # https://www.overkillshop.com/en/sneaker.html?p=2
+    # https://www.fenom.com/fr/2-sneakers?page=2
+    # https://www.urbanindustry.co.uk/collections/shoes?page=3
+    # https://www.titoloshop.com/eu_en/men/sneakers?p=4
+    # https://stockx.com/fr-fr/sneakers?page=4
+    # https://www.flightclub.com/sneakers?page=12
+
+
     home_html = URI.open(home_page_url)
     home_page = Nokogiri::HTML(home_html)
     # find_most_wanted(home_page)
