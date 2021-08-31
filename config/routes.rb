cb54e870d06c1341714a9698ba2aea454de69a57
kirua <yo44prg@icloud.com>
2021-08-31 21:47:20 +0200

    fix: add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :sneakers
   post "/sneakers/delete/:seller" => "sneakers#delete_sneakers_by_seller"
+  post "/sneakers/delete/all" => "sneakers#delete_sneakers_by_seller"
 
   resources :messages
 
