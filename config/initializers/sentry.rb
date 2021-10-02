09d59f7cb57685be15a99b6c93428176523a3d3a
kirua <yo44prg@icloud.com>
2021-10-02 17:06:57 +0200

    feat: post production error log in discord chanel [#40]

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,5 +5,4 @@ Sentry.init do |config|
   config.traces_sample_rate = 1
   config.dsn = 'https://54a0e749a9d44c70b9e13f834b713908@o1023113.ingest.sentry.io/5989423'
   config.release = `git branch --show-current`
-  # config.capture_exception_frame_locals = true
 end
