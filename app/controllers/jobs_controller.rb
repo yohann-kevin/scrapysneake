1e0e2d43ba20023135e9d63f3cf33bc4aca5e275
kirua <yo44prg@icloud.com>
2021-11-18 22:12:04 +0100

    fix: rubocop offense

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
   skip_before_action :authorized
-  
+
   # get last week failed job
   def last_week_failed_job
     jobs = FailedJob.find_last_week_failed_job
--- app/models/failed_job.rb
+++ app/models/failed_job.rb
@@ -5,11 +5,10 @@ class FailedJob < ApplicationRecord
   validates :stack_trace, presence: true
 
   def self.find_last_week_failed_job
-    FailedJob.where("created_at >= ?", 1.week.ago.utc)
     index = 0
     sorted_failed_job = []
-    while index < 8 do
-      sorted_failed_job << { index.day.ago.utc.to_s => FailedJob.where(:created_at => (index+1).day.ago.utc..index.day.ago.utc).count }
+    while index < 8
+      sorted_failed_job << { index.day.ago.utc.to_s => FailedJob.where(created_at: (index + 1).day.ago.utc..index.day.ago.utc).count }
       index += 1
     end
     sorted_failed_job
